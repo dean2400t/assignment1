81234b87c019ad2f91ecd0ba62f843acf0d568a1
dean2400t <dean2400t@gmail.com>
2021-06-06 08:44:00 +0300

    Added puppeteer to capture screenshots

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 
 const app = express();
+const puppeteer = require('puppeteer');
 
-app.get('/', function (req, res) {
+app.get('/', async function (req, res) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://google.com');
+    await page.screenshot({ path: 'example.png' });
+
+    await browser.close();
     return res.status(200).send('Hello World!')
   })
 
-const port=process.env.PORT || 1337;
+const port=process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 module.exports = app;
