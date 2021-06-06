845c3faf043bca32b5d0f474de06e8bc9e3f079c
dean2400t <dean2400t@gmail.com>
2021-06-06 14:43:26 +0300

    take screen shot middleware

---
+++ middleware/takeScreenShot.js
@@ -0,0 +1,18 @@
+//Using puppeteer so screen shots will work for none SSR websites as well
+const puppeteer = require('puppeteer');
+module.exports=async (req,res,next)=>{
+    const screenShotPath='screenShot.png'
+
+    const {url}=req.body;
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    try {
+      await page.goto(url);
+    } catch (error) {
+      return res.status(400).send('URL could no be reached')
+    }
+    await page.screenshot({ path: screenShotPath });
+    await browser.close();
+    req.screenShotPath=screenShotPath
+    next();
+  }
