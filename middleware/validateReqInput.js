1107c147fcd84bde4f6e4ca7ee7935853b1212b6
dean2400t <dean2400t@gmail.com>
2021-06-06 14:43:11 +0300

    validate input middleware

---
+++ middleware/validateReqInput.js
@@ -0,0 +1,18 @@
+
+module.exports= (req, res, next)=>
+{
+  const {url, ...rest}=req.body;
+  if (!url)
+    return res.status(400).send('No url was sent');
+
+  //Checking that there are no other fields in body
+  if (Object.keys(rest).length>0)
+    return res.status(400).send('Object must contain url field only');
+
+  try {
+    new URL(url);
+  } catch (error) {
+    return res.status(400).send(`${error.input} is not a valid url`);
+  }
+  next();
+}
